240d09275cc71d845452ae84fd53f19f5e1abf2d
eddiechapman <eddie.chapman4@gmail.com>
2019-01-28 13:51:29 -0600

    Implement Terrain enum type.

---
+++ src/edu/uwm/cs351/Terrain.java
@@ -0,0 +1,19 @@
+package edu.uwm.cs351;
+
+import java.awt.Color;
+
+public enum Terrain {
+    INACCESSIBLE    (Color.BLACK),
+    WATER           (Color.CYAN),
+    LAND            (Color.WHITE),
+    FOREST          (Color.GREEN),
+    MOUNTAIN        (Color.LIGHT_GRAY),
+    CITY            (Color.ORANGE),
+    DESERT          (Color.YELLOW);
+    
+    private final Color color;
+    Terrain(Color color) {
+      this.color = color;
+    }
+    public Color getColor() { return color; }
+}
