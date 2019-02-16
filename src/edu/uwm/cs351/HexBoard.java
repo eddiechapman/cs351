796a17d2858fbfbd098e35bcdaf48281e023e3ae
John Tang Boyland <boyland@uwm.edu>
2019-02-16 09:22:14 -0600

    Fixed problems.

--- src/edu/uwm/cs351/HexBoard.java
+++ src/edu/uwm/cs351/HexBoard.java
@@ -31,3 +31,49 @@ public class HexBoard extends AbstractCollection<HexTile> {
 
 	@Override
 	public int size() {
+		return 0; // TODO
+	}
+
+	// TODO: What else?
+	
+	/**
+	 * Read a hex board from a reader
+	 * @param br read t read from, must not be null
+	 * @throws FormatException if there is a format error in the final
+	 * @throws IOException if an error reading
+	 */
+	public void read(BufferedReader br) throws FormatException, IOException {
+		String line;
+		while ((line = br.readLine()) != null) {
+			add(HexTile.fromString(line));
+		}
+	}
+	
+	/**
+	 * Write a hex board out.
+	 * @param p print writer to use.
+	 */
+	public void write(PrintWriter p) {
+		for (HexTile h : this) {
+			p.println(h.toString());
+		}
+		p.flush();
+	}
+	
+	private class MyIterator implements Iterator<HexTile> {
+		private Iterator<Map.Entry<HexCoordinate,Terrain>> base = contents.entrySet().iterator();
+		
+		@Override
+		public boolean hasNext() {
+			return false; // TODO: very easy, delegate to the "base" iterator
+		}
+
+		@Override
+		public HexTile next() {
+			return null; // TODO: use base iterator and generate hex tile on demand
+			 */
+		}
+
+		// TODO: what else?
+	}
+}
