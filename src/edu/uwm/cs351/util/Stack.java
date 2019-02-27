66f6d2ec4390ccb6a4d25595a6cdd6fe49dea395
John Tang Boyland <boyland@uwm.edu>
2019-02-27 11:06:43 -0600

    Initial commit

---
+++ src/edu/uwm/cs351/util/Stack.java
@@ -0,0 +1,38 @@
+package edu.uwm.cs351.util;
+
+import java.lang.reflect.Array;
+import java.util.EmptyStackException;
+
+/**
+ * A generic stack class with push/pop methods.
+ * When an instance is created, one may optionally pass in a
+ * class descriptor.  This makes the implementation more robust.
+ * @param T element type of stack
+ */
+public class Stack<T> implements Cloneable {
+
+	private final Class<T> clazz; // initialize to null if necessary
+	
+	// Hint: You are permitted to copy in code from lecture repos.
+	// TODO: Declare fields (for dynamic array data structure)
+	
+	// TODO: declare report/wellFormed
+	
+	// a helper method which you will find useful.
+	@SuppressWarnings("unchecked")
+	private T[] makeArray(int size) {
+		if (clazz == null)
+			return (T[])new Object[size]; // lying...
+		else
+			return (T[])Array.newInstance(clazz, size);
+	}
+	
+	private static final int DEFAULT_CAPACITY = 1;
+
+	// TODO: rest of class
+	// You need two constructors: one taking a class value (used by makeArray)
+	// and one without such a value.  In the former case, makeArray
+	// won't need to lie in its array creation.
+	// Make sure to assert the invariant at end of each constructor
+	// and at start (and end, if they mutate anything) of public methods.
+}
