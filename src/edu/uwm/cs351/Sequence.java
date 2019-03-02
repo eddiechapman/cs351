6e06afb94a3b8e1e16e52ea6508a9a93c6527468
eddiechapman <eddie.chapman4@gmail.com>
2019-03-02 10:39:59 -0600

    Paste in hw4 solution and remove old TODOs.

--- src/edu/uwm/cs351/Sequence.java
+++ src/edu/uwm/cs351/Sequence.java
@@ -5,6 +5,7 @@ package edu.uwm.cs351;
 
 import java.util.Comparator;
 
+import edu.uwm.cs351.HexTileSeq.Node;
 import junit.framework.TestCase;
 
 /******************************************************************************
@@ -66,9 +67,282 @@ public class Sequence<E> implements Cloneable
 	// This is only for testing the invariant.  Do not change!
 	private Sequence(boolean testInvariant) { }
 	
-	// TODO copy code from the Homework #4 HexTileSeq.java (provided)
-	// and convert to use the adjusted and simpler data structure.
+	/**
+     * Initialize an empty sequence.
+     * @param - none
+     * @postcondition
+     *   This sequence is empty.
+     **/   
+    public HexTileSeq( )
+    {
+        assert wellFormed() : "Invariant false at end of constructor";
+    }
 
+    /**
+     * Determine the number of elements in this sequence.
+     * @param - none
+     * @return
+     *   the number of elements in this sequence
+     **/ 
+    public int size( )
+    {
+        assert wellFormed() : "invariant failed at start of size";
+        return manyNodes;
+    }
+
+    /**
+     * Add a new element to this sequence, before the current element. 
+     * @param element
+     *   the new element that is being added, it is allowed to be null
+     * @postcondition
+     *   A new copy of the element has been added to this sequence. If there was
+     *   a current element, then the new element is placed before the current
+     *   element. If there was no current element, then the new element is placed
+     *   at the start of the sequence. In all cases, the new element becomes the
+     *   new current element of this sequence. 
+     **/
+    public void addBefore(HexTile element)
+    {
+        assert wellFormed() : "invariant failed at start of addBefore";
+        if (cursor == null) {
+            precursor = null;
+            cursor = head;
+        }
+        cursor = new Node(element,cursor);
+        if (tail == null) tail = cursor;
+        if (precursor == null) {
+            head = cursor;
+        } else {
+            precursor.next = cursor;
+        }
+        ++manyNodes;
+        assert wellFormed() : "invariant failed at end of addBefore";
+    }
+
+    /**
+     * Add a new element to this sequence, after the current element. 
+     * @param element
+     *   the new element that is being added, may be null
+     * @postcondition
+     *   A new copy of the element has been added to this sequence. If there was
+     *   a current element, then the new element is placed after the current
+     *   element. If there was no current element, then the new element is placed
+     *   at the end of the sequence. In all cases, the new element becomes the
+     *   new current element of this sequence. 
+     **/
+    public void addAfter(HexTile element)
+    {
+        assert wellFormed() : "invariant failed at start of addAfter";
+        if (cursor == null) {
+            tail = cursor = new Node(element,null);
+            if (precursor == null) {
+                head = cursor;
+            } else {
+                precursor.next = cursor;
+            }
+        } else {
+            cursor.next = new Node(element,cursor.next);
+            precursor = cursor;
+            cursor = cursor.next;
+            if (precursor == tail) tail = cursor;
+        }
+        ++manyNodes;
+        assert wellFormed() : "invariant failed at end of addAfter";
+    }
+
+    /// Cursor methods
+    
+    /**
+     * The first element (if any) of this sequence is now current.
+     * @param - none
+     * @postcondition
+     *   The front element of this sequence (if any) is now the current element (but 
+     *   if this sequence has no elements at all, then there is no current 
+     *   element).
+     **/ 
+    public void start( )
+    {
+        assert wellFormed() : "invariant failed at start of start";
+        precursor = null;
+        cursor = head;
+        assert wellFormed() : "invariant failed at end of start";
+    }
+
+    /**
+     * Accessor method to determine whether this sequence has a specified 
+     * current element (a HexTile or null) that can be retrieved with the 
+     * getCurrent method. This depends on the status of the cursor.
+     * @param - none
+     * @return
+     *   true (there is a current element) or false (there is no current element at the moment)
+     **/
+    public boolean isCurrent( )
+    {
+        assert wellFormed() : "invariant failed at start of isCurrent";
+        return cursor != null;
+    }
+
+    /**
+     * Accessor method to get the current element of this sequence. 
+     * @param - none
+     * @precondition
+     *   isCurrent() returns true.
+     * @return
+     *   the current element of this sequence, possibly null
+     * @exception IllegalStateException
+     *   Indicates that there is no current element, so 
+     *   getCurrent may not be called.
+     **/
+    public HexTile getCurrent( )
+    {
+        assert wellFormed() : "invariant failed at start of getCurrent";
+        if (!isCurrent()) throw new IllegalStateException("no current");
+        return cursor.data;
+    }
+
+    /**
+     * Move forward, so that the next element is now the current element in
+     * this sequence.
+     * @param - none
+     * @precondition
+     *   isCurrent() returns true. 
+     * @postcondition
+     *   If the current element was already the end element of this sequence 
+     *   (with nothing after it), then there is no longer any current element. 
+     *   Otherwise, the new current element is the element immediately after the 
+     *   original current element.
+     * @exception IllegalStateException
+     *   If there was no current element, so 
+     *   advance may not be called (the precondition was false).
+     **/
+    public void advance( )
+    {
+        assert wellFormed() : "invariant failed at start of advance";
+        if (!isCurrent()) throw new IllegalStateException("no current");
+        precursor = cursor;
+        cursor = cursor.next;
+        assert wellFormed() : "invariant failed at end of advance";
+    }
+
+    /**
+     * Remove the current element from this sequence.
+     * @param - none
+     * @precondition
+     *   isCurrent() returns true.
+     * @postcondition
+     *   The current element has been removed from this sequence, and the 
+     *   following element (if there is one) is now the new current element. 
+     *   If there was no following element, then there is now no current 
+     *   element.
+     * @exception IllegalStateException
+     *   Indicates that there is no current element, so 
+     *   removeCurrent may not be called. 
+     **/
+    public void removeCurrent( )
+    {
+        assert wellFormed() : "invariant failed at start of removeCurrent";
+        if (!isCurrent()) throw new IllegalStateException("no current to remove");
+        --manyNodes;
+        if (precursor == null) {
+            head = cursor.next;
+        } else {
+            precursor.next = cursor.next;
+        }
+        if (tail == cursor) tail = precursor;
+        cursor = cursor.next;
+        assert wellFormed() : "invariant failed at end of removeCurrent";
+    }
+
+    /**
+     * Place the contents of another sequence at the end of this sequence.
+     * @param addend
+     *   a sequence whose contents will be placed at the end of this sequence
+     * @precondition
+     *   The parameter, addend, is not null. 
+     * @postcondition
+     *   The elements from addend have been placed at the end of 
+     *   this sequence. The current element of this sequence if any,
+     *   remains unchanged.   The addend is unchanged.
+     * @exception NullPointerException
+     *   Indicates that addend is null. 
+     **/
+    public void addAll(HexTileSeq addend)
+    {
+        assert wellFormed() : "invariant failed at start of addAll";
+        if (addend.manyNodes == 0) return;
+        HexTileSeq copy = addend.clone();
+        if (tail == null) {
+            head = copy.head;
+        } else {
+            tail.next = copy.head;
+        }
+        tail = copy.tail;
+        if (cursor == null) {
+            precursor = copy.tail;
+        }
+        manyNodes += addend.manyNodes;
+        assert wellFormed() : "invariant failed at end of addAll";
+    }   
+
+
+    /**
+     * Generate a copy of this sequence.
+     * @param - none
+     * @return
+     *   The return value is a copy of this sequence. Subsequent changes to the
+     *   copy will not affect the original, nor vice versa.
+     * @exception OutOfMemoryError
+     *   Indicates insufficient memory for creating the clone.
+     **/ 
+    public HexTileSeq clone( )
+    {  // Clone a HexTileSeq object.
+        assert wellFormed() : "invariant failed at start of clone";
+        HexTileSeq answer;
+
+        try
+        {
+            answer = (HexTileSeq) super.clone( );
+        }
+        catch (CloneNotSupportedException e)
+        {  // This exception should not occur. But if it does, it would probably
+            // indicate a programming error that made super.clone unavailable.
+            // The most common error would be forgetting the "Implements Cloneable"
+            // clause at the start of this class.
+            throw new RuntimeException
+            ("This class does not implement Cloneable");
+        }
+        
+        Node dummy = new Node(null,null);
+        Node newTail = dummy;
+        for (Node p = head; p != null; p = p.next) {
+            Node c = new Node(p.data,null);
+            newTail = newTail.next = c;
+            if (p == precursor) answer.precursor = c;
+            if (p == cursor) answer.cursor = c;
+            if (p == tail) answer.tail = c;
+        }
+        answer.head = dummy.next;
+        
+        assert wellFormed() : "invariant failed at end of clone";
+        assert answer.wellFormed() : "invariant failed for clone";
+        
+        return answer;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        boolean first = true;
+        for (Node p = head; p != null; p=p.next) {
+            if (first) first = false;
+            else sb.append(", ");
+            if (p == cursor) sb.append("*");
+            sb.append(p.data);
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+    
 	// TODO:
 	// Add the "sort" method with documentation comment (AKA "javadoc").
 	
