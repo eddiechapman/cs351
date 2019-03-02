17eb7ea2218fb3018e2b6e01de78a00d03c267b7
eddiechapman <eddie.chapman4@gmail.com>
2019-03-02 12:38:38 -0600

    Implement start.

--- src/edu/uwm/cs351/Sequence.java
+++ src/edu/uwm/cs351/Sequence.java
@@ -199,8 +199,7 @@ public class Sequence<E> implements Cloneable
     public void start( )
     {
         assert wellFormed() : "invariant failed at start of start";
-        precursor = null;
-        cursor = head;
+        precursor = dummy;
         assert wellFormed() : "invariant failed at end of start";
     }
 
