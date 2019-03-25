915a66d3f2be857c63ce0227c6ff5d0eb67683dc
John Tang Boyland <boyland@uwm.edu>
2019-03-24 20:41:27 -0500

    Initial commit

---
+++ src/edu/uwm/cs351/HexBoard.java
@@ -0,0 +1,111 @@
+package edu.uwm.cs351;
+
+import java.util.AbstractCollection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+import junit.framework.TestCase;
+
+/**
+ * An implementation of the HexBoard ADT using 
+ * a binary search tree implementation.
+ * A hex board is a collection of hex tiles except that there can 
+ * never be two tiles at the same location. 
+ */
+public class HexBoard extends AbstractCollection<HexTile> {
+
+	private static int compare(HexCoordinate h1, HexCoordinate h2) {
+		if (h1.b() == h2.b()) {
+			return h1.a() - h2.a();
+		}
+		return h1.b() - h2.b();
+	}
+	
+	private static class Node {
+		HexCoordinate loc;
+		Terrain terrain;
+		Node left, right;
+		Node(HexCoordinate l, Terrain t) { loc = l; terrain = t; }
+	}
+	
+	private Node root;
+	private int size;
+	private int version;
+	
+	private static boolean doReport = true; 
+	private static boolean report(String s) {
+		if (doReport) System.err.println("Invariant error: " + s);
+		else System.out.println("Detected invariant error: " + s);
+		return false;
+	}
+	
+	/**
+	 * Return true if the nodes in this BST are properly
+	 * ordered with respect to the {@link #compare(HexCoordinate, HexCoordinate)}
+	 * method.  If a problem is found, it should be reported (once).
+	 * @param r subtree to check (may be null)
+	 * @param lo lower bound (if any)
+	 * @param hi upper bound (if any)
+	 * @return whether there are any problems in the tree.
+	 */
+	private static boolean isInProperOrder(Node r, HexCoordinate lo, HexCoordinate hi) {
+		if (r == null) return true;
+		if (r.loc == null) return report("null location in tree");
+		if (r.terrain == null) return report("null terrain for " + r.loc);
+		if (lo != null && compare(lo,r.loc) >= 0) return report("out of order " + r.loc + " <= " + lo);
+		if (hi != null && compare(hi,r.loc) <= 0) return report("out of order " + r.loc + " >= " + hi);
+		return isInProperOrder(r.left,lo,r.loc) && isInProperOrder(r.right,r.loc,hi);
+	}
+	
+	/**
+	 * Return the count of the nodes in this subtree.
+	 * @param p subtree to count nodes for (may be null)
+	 * @return number of nodes in the subtree.
+	 */
+	private static int countNodes(Node p) {
+		if (p == null) return 0;
+		return 1 + countNodes(p.left) + countNodes(p.right);
+	}
+	
+	private boolean wellFormed() {
+		if (!isInProperOrder(root,null,null)) return false;
+		int count = countNodes(root);
+		if (size != count) return report("size " + size + " wrong, should be " + count);
+		return true;
+	}
+	
+	/**
+	 * Create an empty hex board.
+	 */
+	public HexBoard() {
+		root = null;
+		size = 0;
+		assert wellFormed() : "in constructor";
+	}
+	
+	/** Return the terrain at the given coordinate or null
+	 * if nothing at this coordinate.
+	 * @param c hex coordinate to look for (null OK but pointless)
+	 * @return terrain at that coordinate, or null if nothing
+	 */
+	public Terrain terrainAt(HexCoordinate l) {
+		assert wellFormed() : "in terrainAt";
+		for (Node p = root; p != null; ) {
+			int c = compare(l,p.loc);
+			if (c == 0) return p.terrain;
+			if (c < 0) p = p.left;
+			else p = p.right;
+		}
+		return null;
+	}
+
+	@Override // required by Java
+	public Iterator<HexTile> iterator() {
+		assert wellFormed() : "in iterator";
+		return new MyIterator();
+	}
+
+	@Override // required by Java
+	public int size() {
