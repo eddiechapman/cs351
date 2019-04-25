f9221e86c944cba4eb23d310b9e48add0c9477c9
eddiechapman <eddie.chapman4@gmail.com>
2019-04-25 15:07:00 -0500

    LIFOWorklist class.

---
+++ src/edu/uwm/cs351/LIFOWorklist.java
@@ -0,0 +1,42 @@
+package edu.uwm.cs351;
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+import edu.uwm.cs351.util.Worklist;
+
+/**
+ * A Worklist whose most recent addition is the first to be accessed.
+ * 
+ * @author Eddie Chapman
+ * @param <E>       The type of element to be stored in the Worklist.
+ */
+public class LIFOWorklist<E> implements Worklist<E> {
+    private Stack<E> worklist; 
+    
+    /**
+     * Initialize a new LIFOWorklist.
+     * 
+     * @postcondition   The Worklist is empty.
+     */
+    public LIFOWorklist() {
+        worklist = new Stack<E>();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !worklist.empty();
+    }
+
+    @Override
+    public E next() {
+        if (!hasNext()) throw new NoSuchElementException("Exhausted");
+        return worklist.pop();
+    }
+
+    @Override
+    public void add(E element) {
+        worklist.push(element);    
+    }
+
+}
